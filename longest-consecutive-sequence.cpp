1d0ed647cbd7189e2202169bb4707be8c8d20f02
202401005 <202401005@daiict.ac.in>
2025-04-03 19:44:10 +0530

    Update longest-consecutive-sequence.cpp

--- longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int>st(nums.begin(),nums.end());
+        int maximum=1;
+        if(nums.size()==0){
+            return 0;
+        }
+        for(auto x:st){
+            if(!st.count(x-1)){
+                int c=1;
+                int z=x;
+                while(st.count(z+1)){
+                    c++;
+                    z++;
+                }
+                maximum=max(maximum,c);
+            }
+        }
+        return maximum;
+        
+    }
+};
 
